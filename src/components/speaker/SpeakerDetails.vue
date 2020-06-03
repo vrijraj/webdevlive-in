56605c115345b9d674bceb32750a31a8be1a0fb9
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-03 23:45:46 +0530

    Speakers data update

---
+++ src/components/speaker/SpeakerDetails.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-dialog v-model="dialog" width="700">
+    <template v-slot:activator="{ on }">
+      <div
+        v-on="on"
+        style="cursor: pointer;"
+        class="text-center py-5 ma-1 fill-height"
+        :class="$vuetify.theme.dark == true ? 'darkModeCard' : 'whiteThemeCard'"
+      >
+        <v-avatar size="100">
+          <img :src="getImgUrl(data.image)" :lazy-src="getImgUrl(data.image)" alt />
+        </v-avatar>
+        <p class="mt-3 mb-0 google-font mb-0" style="font-size:120%">{{ data.name }}</p>
+        <p class="mt-1 mb-0 google-font mt-0" style="font-size:80%">{{ data.company.name }}</p>
+        <socialMediaDetails :data="data.social" />
+      </div>
+    </template>
+
+    <v-card :class="theme.isDark ? 'grey darken-3' : 'white'">
+      <!-- <v-card-title class="px-5 google-font" primary-title>
+        {{ data.name }} Details
+      </v-card-title>-->
+
+      <v-card-text class="pa-5">
+        <v-container>
+          <v-row>
+            <v-col md="4" cols="12" class="text-center">
+              <v-avatar size="100">
+                <img :src="getImgUrl(data.image)" :lazy-src="getImgUrl(data.image)" alt />
+              </v-avatar>
+              <p class="google-font mt-3 mb-0" style="font-size:140%">
+                <b>{{ data.name }}</b>
+              </p>
+              <p class="google-font mt-1" style="font-size:100%">
+                {{ data.company.designation }}
+                <span v-if="data.company.name">,&nbsp;</span>
+                {{ data.company.name }}
+              </p>
+              <socialMediaDetails class="pl-0 ml-0" :data="data.social" />
+            </v-col>
+            <v-col md="8" cols="12">
+              <p class="google-font" style="font-size:110%">{{ data.bio }}</p>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import socialMediaDetails from "@/components/common/SocialInfo";
+export default {
+  components: {
+    socialMediaDetails
+  },
+  inject: ["theme"],
+  props: ["data"],
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  mounted() {},
+  filters: {
+    summary: (val, num) => {
+      if (val.length > num) return val.substring(0, num) + "..";
+      else return val;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.darkModeCard {
+  background-color: #292929;
+  border: 1px solid #212121;
+  border-radius: 5px;
+}
+.whiteThemeCard {
+  background: white;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+}
+</style>
--- src/views/Speakers.vue
+++ src/views/Speakers.vue
@@ -1,11 +1,79 @@
 <template>
   <v-content>
-    <v-container>
-      <v-row>
-        <v-col>
-          <h1 class="google-font">Speakers Page</h1>
+    <v-container
+      fluid
+      class="px-1"
+      :class="this.$vuetify.theme.dark == true?'grey darken-4':'grey lighten-4'"
+    >
+      <v-row justify="center" align="center">
+        <v-col md="10" lg="10" sm="11" cols="12">
+          <v-container>
+            <v-row class>
+              <v-col class="pb-5">
+                <h1 class="google-font mb-5">Our Speakers</h1>
+                <br />
+                <br />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid class="px-1">
+      <v-row justify="center" align="center" class="px-0">
+        <v-col md="10" lg="10" sm="11" cols="12" class="card-top-margin">
+          <v-container>
+            <v-row class>
+              <v-col
+                md="3"
+                lg="3"
+                xl="2"
+                sm="4"
+                cols="6"
+                class="px-1"
+                v-for="(item, index) in SpeakersData"
+                :key="index"
+              >
+                <Speakers :data="item" />
+              </v-col>
+            </v-row>
+          </v-container>
         </v-col>
       </v-row>
     </v-container>
   </v-content>
 </template>
+
+<script>
+import Speakers from "@/components/speaker/SpeakerDetails";
+import SpeakersData from "@/assets/data/speakers.json";
+export default {
+  name: "SpeakersPage",
+  components: {
+    Speakers
+  },
+  data: () => ({
+    SpeakersData: SpeakersData.sort((a, b) => a.name - b.name)
+  }),
+  mounted() {
+    this.SpeakersData.sort((a, b) => {
+      const x = a.name.toLowerCase();
+      const y = b.name.toLowerCase();
+      if (x < y) {
+        return -1;
+      }
+      if (x > y) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+};
+</script>
+
+<style>
+.card-top-margin {
+  margin-top: -150px;
+}
+</style>
