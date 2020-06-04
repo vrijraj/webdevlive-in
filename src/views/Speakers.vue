2dfc183a908a3d5331d065537bf39561f6f96eb6
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-05 02:17:00 +0530

    Design update

--- src/views/Speakers.vue
+++ src/views/Speakers.vue
@@ -17,7 +17,7 @@
                 
                 <v-img :src="require('@/assets/img/webdevlogo.png')" style="margin-left:auto;margin-right:auto" width="5vh"></v-img>
                 <h1 class="google-font mb-0" style="font-size:180%">Check out our speaker lineup</h1>
-                <p class="google-font" style="width:100%;margin-left:auto;margin-right:auto;font-size:110%">Whatever your challenge, see how these leaders on the front line of transformation, innovation, and exploration can help solve it with you. Get to know the inspiring minds right here.</p>
+                <p class="google-font" style="width:70%;margin-left:auto;margin-right:auto;font-size:110%">Whatever your challenge, see how these leaders on the front line of transformation, innovation, and exploration can help solve it with you. Get to know the inspiring minds right here.</p>
               </v-col>
             </v-row>
           </v-container>
