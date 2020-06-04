42fda0ae7f0a9b21ad4435355dc8c00e4f576c26
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-04 18:19:14 +0530

    Components added

---
+++ src/components/home/eventContext.vue
@@ -0,0 +1,125 @@
+<template>
+  <v-container fluid class>
+    <v-layout
+      wrap
+      align-center
+      justify-center
+      row
+      fill-height
+      class="hidden-sm-and-down ma-2 what-do-wrapper"
+      style
+    >
+      <v-flex xs12 sm5 md3 lg3 xl3 class="text-center pa-5">
+        <v-hover v-slot:default="{ hover }">
+          <v-card class="pa-3" :elevation="hover ? 0 : 0">
+            <v-icon size="50px">mdi-microphone</v-icon>
+            <p class="google-font my-0" style="font-size:150%;color:#424242">Sessions</p>
+            <br />
+            <p
+              class="google-font"
+              style="font-size:100%;color:#9e9e9e"
+            >A plethora of talks ranging from technologies like ML, Cloud, Android and Web to Design to Entrepreneurship.</p>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-flex xs12 sm5 md3 lg3 xl3 class="text-center pa-5">
+        <v-hover v-slot:default="{ hover }">
+          <v-card class="pa-3" :elevation="hover ? 0 : 0">
+            <v-icon size="50px">mdi-lightbulb-on</v-icon>
+            <p class="google-font my-0" style="font-size:150%;color:#424242">Ice Breakers</p>
+            <br />
+            <p
+              class="google-font"
+              style="font-size:100%;color:#9e9e9e"
+            >A series of engaging online activities in between the sessions to combine learning and fun.</p>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-flex xs12 sm5 md3 lg3 xl3 class="text-center pa-5">
+        <v-hover v-slot:default="{ hover }">
+          <v-card class="pa-3" :elevation="hover ? 0 : 0">
+            <v-icon size="50px">mdi-access-point-network</v-icon>
+            <p class="google-font my-0" style="font-size:150%;color:#424242">Stories</p>
+            <br />
+            <p
+              class="google-font"
+              style="font-size:100%;color:#9e9e9e"
+            >Hear from the community champions about their stories, experiences and learnings.</p>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-flex xs12 sm5 md3 lg3 xl3 class="text-center pa-5">
+        <v-hover v-slot:default="{ hover }">
+          <v-card class="pa-3" :elevation="hover ? 0 : 0">
+            <v-icon size="50px">mdi-nfc-tap</v-icon>
+            <p class="google-font my-0" style="font-size:150%;color:#424242">Discussions</p>
+            <br />
+            <p
+              class="google-font"
+              style="font-size:100%;color:#9e9e9e"
+            >A variety of discussions through a Panel discussions, Fireside chat and many online conversations.</p>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    </v-layout>
+
+    <v-layout
+      wrap
+      align-center
+      justify-center
+      row
+      fill-height
+      class="hidden-sm-and-up"
+      style="padding-top:5%;padding-bottom:5%;background:#fafafa"
+    >
+      <v-flex xs12 sm5 md3 lg3 xl3 class="px-5">
+        <span
+          class="pa-2 google-font"
+          style="border-radius: 7px; background-color: rgb(210, 227, 252); color: rgb(23, 78, 166); font-size: 130%;"
+        >Sessions</span>
+        <p
+          class="google-font mt-3"
+          style="font-size:100%;color:#9e9e9e"
+        >A plethora of talks ranging from technologies like ML, Cloud, Android and Web to Design to Entrepreneurship.</p>
+      </v-flex>
+      <v-flex xs12 sm5 md3 lg3 xl3 class="px-5 mt-2">
+        <span
+          class="pa-2 google-font"
+          style="border-radius: 7px; background-color: rgb(254, 247, 224); color: rgb(176, 96, 0); font-size: 120%;"
+        >Ice Breakers</span>
+        <p
+          class="google-font mt-3"
+          style="font-size:100%;color:#9e9e9e"
+        >A series of engaging online activities in between the sessions to combine learning and fun.</p>
+      </v-flex>
+      <v-flex xs12 sm5 md3 lg3 xl3 class="px-5 mt-2">
+        <span
+          class="pa-2 google-font"
+          style="border-radius: 7px; background-color: rgb(252, 232, 230); color: rgb(179, 20, 18); font-size: 120%;"
+        >Stories</span>
+        <p
+          class="google-font mt-3"
+          style="font-size:100%;color:#9e9e9e"
+        >Hear from the community champions about their stories, experiences and learnings.</p>
+      </v-flex>
+      <v-flex xs12 sm5 md3 lg3 xl3 class="px-5 mt-2">
+        <span
+          class="pa-2 google-font"
+          style="border-radius: 7px; background-color: rgb(206, 234, 214); color: rgb(13, 101, 45); font-size: 120%;"
+        >Discussions</span>
+        <p
+          class="google-font mt-3"
+          style="font-size:100%;color:#9e9e9e"
+        >A variety of discussions through a Panel discussions, Fireside chat and many online conversations.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/components/home/stats.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container fluid class="mb-0 pb-0">
+    <v-layout wrap align-center justify-space-between row fill-height>
+      <v-flex xs12 sm5 md7 lg7 xl6 class="pt-5">
+        <p class="google-font mb-1 stats-wrapper" style="font-weight: 350;color: indigo;font-size:200%">
+          <b class="google-font">Everyone is welcome here</b>
+        </p>
+        <p class="google-font mt-3" style="text-align:justify;">
+          Inviting women and allies from India and abroad who wish to celebrate and recognize the women who have influenced the tech industry and beyond.
+          <span class="hidden-sm-and-down hidden-md-and-down">
+            Together let’s celebrate the super-women in our lives. Together let’s create a new narrative for our industry.
+            <br />
+          </span>
+          <br />
+          <span class="hidden-sm-and-down hidden-md-and-down">
+            Allies are people who work to support diverse groups in our community with which they may not necessarily identify as members.
+            <br />
+          </span>
+          <br />This event is put together by a group of amazing, passionate women who truly believe in nurturing communities.
+          <span
+            class="hidden-sm-and-down hidden-md-and-down"
+          >Our speakers are women who have been associated with communities within their organisations, cities, country and beyond.</span>
+        </p>
+      </v-flex>
+
+      <v-flex xs12 sm5 md4 lg4 xl6 class="pt-5">
+        <v-layout row wrap class="px-5">
+          <v-flex xs6>
+            <p class="google-font mb-0" style="font-weight: 350;color: #616161;font-size:300%">
+              <b>
+                <span style="color: indigo;">0</span>
+              </b>
+            </p>
+            <p class="google-font">Registrations</p>
+          </v-flex>
+
+          <v-flex xs6>
+            <p class="google-font mb-0" style="font-weight: 350;color: #616161;font-size:300%">
+              <b>
+                <span style="color: indigo;">0</span>
+              </b>
+            </p>
+            <p class="google-font">Communities</p>
+          </v-flex>
+          <v-flex xs6>
+            <p class="google-font mb-0" style="font-weight: 350;color: #616161;font-size:300%">
+              <b>
+                <span style="color: indigo;">0</span>
+              </b>
+            </p>
+            <p class="google-font">Speakers</p>
+          </v-flex>
+          <v-flex xs6>
+            <p class="google-font mb-0" style="font-weight: 350;color: #616161;font-size:300%">
+              <b>
+                <span style="color: indigo;">0</span>
+              </b>
+            </p>
+            <p class="google-font">Sessions</p>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
