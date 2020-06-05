7ee409e2db8f6ec23d0097e13cb632a9e4955cf8
Vrijraj Singh <vrijraj2396@gmail.com>
2020-06-06 01:19:02 +0530

    Home page tab ui color updated

--- src/components/home/overview.vue
+++ src/components/home/overview.vue
@@ -9,6 +9,7 @@
           :grow="getMobileTab()"
           :centered="true"
           slider-color="indigo"
+          :background-color="$vuetify.theme.dark ? 'grey darken-4' : 'grey lighten-4'"
           style="border-bottom: 1px solid #e0e0e0;"
         >
           <v-tab
