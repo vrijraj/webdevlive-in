f13a7f2598e7b8b07ae3199883a18042c596b73d
Vrijraj Singh <vrijraj2396@gmail.com>
2020-06-10 02:51:55 +0530

    Schedule Code changed

--- src/components/Schedule/ScheduleDetails.vue
+++ src/components/Schedule/ScheduleDetails.vue
@@ -5,7 +5,7 @@
       align="center"
       v-for="(item, index) in data.schedule"
       :key="index"
-      :class="$vuetify.theme.dark?'row-border-dark ':'row-border-white'"
+      :class="$vuetify.theme.dark ? 'row-border-dark ' : 'row-border-white'"
       class="pa-0 my-0 "
     >
       <v-col md="2" cols="3" class="text-right my-0 py-0">
@@ -19,7 +19,11 @@
       </v-col>
       <v-col
         class="my-0"
-        :class="$vuetify.theme.dark?'schedule-details-dark col-border-dark':'schedule-details-white col-border-white'"
+        :class="
+          $vuetify.theme.dark
+            ? 'schedule-details-dark col-border-dark'
+            : 'schedule-details-white col-border-white'
+        "
         cols="9"
         md="10"
       >
@@ -30,43 +34,42 @@
 </template>
 
 <script>
-import ScheduleDialog from '@/components/Schedule/ScheduleDialog'
-import SessionsData from '@/assets/data/sessions.json'
-    export default {
-        props:['data'],
-        components:{
-            ScheduleDialog
-        },
-        data:()=>({
-            SessionsData:SessionsData,
-        }),
-        methods:{
-            getSessionData(id){
-                return this.SessionsData.filter(sd=>sd.id==id)[0]
-            }
-        }
-
-    }
+import ScheduleDialog from "@/components/Schedule/ScheduleDialog";
+import SessionsData from "@/assets/data/sessions.json";
+export default {
+  props: ["data"],
+  components: {
+    ScheduleDialog,
+  },
+  data: () => ({
+    SessionsData: SessionsData,
+  }),
+  methods: {
+    getSessionData(id) {
+      return this.SessionsData.filter((sd) => sd.id == id)[0];
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .schedule-details-white:hover{
-      background: #FAFAFA;
-    }
-    .schedule-details-dark:hover{
-      background: #433E47;
-    }
-    .row-border-white{
-        border-bottom:1px solid #e0e0e0
-    }
-    .row-border-dark{
-        border-bottom:1px solid #292929;
-        background: black;
-    }
-    .col-border-white{
-        border-left:1px solid #e0e0e0
-    }
-    .col-border-dark{
-        border-left:1px solid #292929
-    }
+.schedule-details-white:hover {
+  background: #fafafa;
+}
+.schedule-details-dark:hover {
+  background: #433e47;
+}
+.row-border-white {
+  border-bottom: 1px solid #e0e0e0;
+}
+.row-border-dark {
+  border-bottom: 1px solid #292929;
+  background: black;
+}
+.col-border-white {
+  border-left: 1px solid #e0e0e0;
+}
+.col-border-dark {
+  border-left: 1px solid #292929;
+}
 </style>
