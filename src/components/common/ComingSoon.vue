20f24df18f580abae59ca6614d15d624d3fc3eb3
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-14 03:36:45 +0530

    UI and data update

--- src/components/common/ComingSoon.vue
+++ src/components/common/ComingSoon.vue
@@ -1,23 +1,30 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
-                <v-img :src="require('@/assets/img/webdevlogo.png')" style="margin-left:auto;margin-right:auto" width="5vh"></v-img>
-                <h1 :class="this.$vuetify.theme.dark?'white--text':'grey--text text--darken-2'" class="google-font mb-0" style="font-size:180%">{{title}}</h1>
-                <p class="google-font" style="width:80%;margin-left:auto;margin-right:auto;font-size:110%">{{des}}</p>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container fluid class>
+    <v-row>
+      <v-col>
+        <h1
+          :class="this.$vuetify.theme.dark?'white--text':'grey--text text--darken-2'"
+          class="google-font mb-0"
+          style="font-size:180%"
+        >Coming Soon...</h1>
+        <p
+          class="google-font"
+          style="width:80%;font-size:110%"
+        >The team is working on finalizing the data. Please have a look after 20th June, 2020 for the updated information.</p>
+        <!-- <v-img :src="require('@/assets/img/webdevlogo.png')" style="margin-left:auto;margin-right:auto" width="5vh"></v-img> -->
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <SocialShare />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        data:()=>({
-
-        }),
-        props:{
-            title:'',
-            des:''
-        }
-    }
+export default {
+  data: () => ({}),
+  components: {
+    SocialShare: () => import("@/components/common/SocialShare")
+  }
+};
 </script>
