1143552987098a84f63ffef7c9a0d269ade1f35c
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-14 13:12:01 +0530

    Registration route update

--- src/components/home/StartScreen.vue
+++ src/components/home/StartScreen.vue
@@ -3,7 +3,7 @@
     <v-row align="center" justify="center">
       <v-col cols="12" md="5" lg="4" class="text-center">
         <v-img :src="require('@/assets/img/home.png')">
-         <template v-slot:placeholder>
+          <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
             </v-row>
@@ -11,7 +11,10 @@
         </v-img>
       </v-col>
       <v-col cols="12" md="7" lg="7">
-        <h1 class="google-font mt-5 mb-0" :class="this.$vuetify.theme.dark?'white--text':'grey--text text--darken-2'">web.dev Live India</h1>
+        <h1
+          class="google-font mt-5 mb-0"
+          :class="this.$vuetify.theme.dark?'white--text':'grey--text text--darken-2'"
+        >web.dev Live India</h1>
         <p class="google-font mt-0" style="font-size:80%">
           Follow up event of
           <a rel="noreferrer" href="https://web.dev/live/" target="_blank">web.dev</a>
@@ -39,28 +42,27 @@
             <!-- &nbsp; -->
           </span>
         </p>
-        <v-btn
-          v-if="mainData.links.registration.length>0"
-          aria-label="Home BTN"
-          color="primary"
-          style="text-transform: capitalize;"
-          class="google-font"
-          depressed
-          large
-          target="_blank"
-          :href="mainData.links.registration"
-          dark
-        >Registration</v-btn>
+        <router-link to="/register" target="_blank">
+          <v-btn
+            aria-label="Home BTN"
+            color="primary"
+            style="text-transform: capitalize;"
+            class="google-font"
+            depressed
+            large
+            dark
+          >Registration</v-btn>
+        </router-link>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-  import aboutEvent from "@/assets/data/about.json";
-  export default {
-    data: ()=>({
-      mainData:aboutEvent
-    })
-  }
+import aboutEvent from "@/assets/data/about.json";
+export default {
+  data: () => ({
+    mainData: aboutEvent
+  })
+};
 </script>
