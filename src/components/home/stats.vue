c4554d58889920e1788ecadf98bdca5e3d50a21d
Vrijraj Singh <vrijraj2396@gmail.com>
2020-06-16 05:10:42 +0530

    reg count inc

--- src/components/home/stats.vue
+++ src/components/home/stats.vue
@@ -18,7 +18,7 @@
           <v-col md="6" class="text-center" cols="6">
             <p class="google-font mb-0" style="font-weight: 350;color: #616161;font-size:300%">
               <b>
-                <span class="primary--text">100+</span>
+                <span class="primary--text">300+</span>
               </b>
             </p>
             <p class="google-font">Registrations</p>
